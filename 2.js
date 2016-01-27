34ebde3cfc7a41e969fc9dbae9cd0b756a16f512
Алина Канивец < >
2016-01-27 19:31:32 +0200

    3

--- 2.js
+++ 2.js
@@ -1,10 +1,10 @@
 var i
 for (i=1; i<=100; i++)
 {
-	if (i%3) {console.log("Fiz")} 
+	if (i%3==0) {console.log("Fiz")} 
 		else
-			if (i%5) {console.log("Buzz")}
+			if (i%5==0) {console.log("Buzz")}
 		else
-			if (i%15) {console.log("FizzBuzz")}
+			if (i%15==0) {console.log("FizzBuzz")}
 		else {console.log(i)}
 }
